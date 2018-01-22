bf9fa185081edac9099762e5e2aa50711ebc8143
unknown <deshanpraveendra@gmail.com>
2018-01-22 10:39:37 +0530

    Saving ontology in the project directory

--- AutoQA/views.py
+++ AutoQA/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .compromise import *
+import os
+import sys
+import cv2
+import numpy as np
+import argparse
+import matplotlib.pyplot as plt
+from datetime import datetime
+from owlready2 import *
+import urllib.request
+import shutil
+
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 def index(request):
     str = "Hello this is view index method </br>"
@@ -10,4 +23,17 @@ def index(request):
         str02 = classCompromise.compromise()
         str = str + "</br> </br>" + str02
 
-    return HttpResponse (str)
+    target = os.path.join(APP_ROOT, 'ExampleOntology/')
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    onto_path.append(target)
+    onto = get_ontology("http://www.lesfleursdunormal.fr/static/_downloads/pizza_onto.owl")
+    onto.load()
+    #onto.save(target, "ont")
+
+    destination = "/".join([target, "ontol"])
+    with urllib.request.urlopen("http://www.lesfleursdunormal.fr/static/_downloads/pizza_onto.owl") as response, open("Onto", 'wb') as destination:
+        shutil.copyfileobj(response, destination)
+
+    return HttpResponse(str)
